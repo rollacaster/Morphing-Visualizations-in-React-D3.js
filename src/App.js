4854a5ebdba8376ef224ed1779a3a89f20865997
Thomas Sojka <thomas@Thomass-MBP.fritz.box>
2020-04-13 12:36:00 +0200

    Project Setup
    Create a new project with create-react-app and cleanup of the generated files.
    
    🎥 Next video: https://youtu.be/Vmulxe7KsYc

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ paddingLeft: '2rem' }}>
+      <h1>Morphing Viz with D3.js & React</h1>
+    </div>
+  )
+}
+
+export default App
