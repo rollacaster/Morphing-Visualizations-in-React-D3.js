4f6e58021b1d6a897f8b4d5f9fd0e1a282b3094f
Thomas Sojka <thsojka@web.de>
2020-04-18 16:22:26 +0200

    Create Transitions
    Transition between the nodes of a tree and the nodes of the pack viz.
    
    🎥 Next video: https://youtu.be/1pIslxUh11s

--- src/App.js
+++ src/App.js
@@ -28,6 +28,13 @@ const animalsPack = createPack(animalsHierarchy()).descendants()
 const createTree = tree().size([500, 480])
 const animalsTree = createTree(animalsHierarchy())
 
+const prepareData = () => {
+  return animalsPack.map((packItem, i) => ({
+    packItem,
+    treeItem: animalsTree.descendants()[i]
+  }))
+}
+
 function App() {
   const [toggle, setToggle] = useState(true)
   return (
@@ -35,45 +42,28 @@ function App() {
       <h1>Morphing Viz with D3.js & React</h1>
       <button onClick={() => setToggle(!toggle)}>Morph</button>
       <svg width="500" height="500">
-        {toggle ? (
-          animalsPack.map(({ x, y, r, data: { name } }) => (
-            <circle
-              key={name}
-              cx={x}
-              cy={y}
-              r={r}
-              fill="transparent"
-              stroke="black"
-            />
-          ))
-        ) : (
-          <g transform="translate(0, 10 )">
-            {animalsTree
-              .links()
-              .map(
-                ({ source: { x: x1, y: y1 }, target: { x: x2, y: y2 } }, i) => (
-                  <line
-                    key={i}
-                    x1={x1}
-                    y1={y1}
-                    x2={x2}
-                    y2={y2}
-                    stroke="black"
-                  />
-                )
-              )}
-            {animalsTree.descendants().map(({ x, y, data: { name } }) => (
+        <g transform="translate(0, 10 )">
+          {prepareData().map(
+            ({
+              treeItem: {
+                x,
+                y,
+                data: { name }
+              },
+              packItem: { x: cx, y: cy, r }
+            }) => (
               <circle
+                style={{ transition: 'all 3s' }}
                 key={name}
-                cx={x}
-                cy={y}
-                r={10}
+                cx={toggle ? x : cx}
+                cy={toggle ? y : cy}
+                r={toggle ? 10 : r}
                 fill="transparent"
                 stroke="black"
               />
-            ))}
-          </g>
-        )}
+            )
+          )}
+        </g>
       </svg>
     </div>
   )
