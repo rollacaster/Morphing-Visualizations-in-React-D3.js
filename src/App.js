b5e5d437d9201a2be30801814b79bfb87b4f1359
Thomas Sojka <thsojka@web.de>
2020-04-18 16:21:09 +0200

    Add Morph Button
    Add a button which switches toggles between the currently displayed
    visualization.
    
    🎥 Next video: https://youtu.be/787vdrnoXw0

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { pack, hierarchy, tree } from 'd3-hierarchy'
 
@@ -29,39 +29,51 @@ const createTree = tree().size([500, 480])
 const animalsTree = createTree(animalsHierarchy())
 
 function App() {
+  const [toggle, setToggle] = useState(true)
   return (
     <div style={{ paddingLeft: '2rem' }}>
       <h1>Morphing Viz with D3.js & React</h1>
+      <button onClick={() => setToggle(!toggle)}>Morph</button>
       <svg width="500" height="500">
-        {animalsPack.map(({ x, y, r, data: { name } }) => (
-          <circle
-            key={name}
-            cx={x}
-            cy={y}
-            r={r}
-            fill="transparent"
-            stroke="black"
-          />
-        ))}
-        <g transform="translate(0, 10 )">
-          {animalsTree
-            .links()
-            .map(
-              ({ source: { x: x1, y: y1 }, target: { x: x2, y: y2 } }, i) => (
-                <line key={i} x1={x1} y1={y1} x2={x2} y2={y2} stroke="black" />
-              )
-            )}
-          {animalsTree.descendants().map(({ x, y, data: { name } }) => (
+        {toggle ? (
+          animalsPack.map(({ x, y, r, data: { name } }) => (
             <circle
               key={name}
               cx={x}
               cy={y}
-              r={10}
+              r={r}
               fill="transparent"
               stroke="black"
             />
-          ))}
-        </g>
+          ))
+        ) : (
+          <g transform="translate(0, 10 )">
+            {animalsTree
+              .links()
+              .map(
+                ({ source: { x: x1, y: y1 }, target: { x: x2, y: y2 } }, i) => (
+                  <line
+                    key={i}
+                    x1={x1}
+                    y1={y1}
+                    x2={x2}
+                    y2={y2}
+                    stroke="black"
+                  />
+                )
+              )}
+            {animalsTree.descendants().map(({ x, y, data: { name } }) => (
+              <circle
+                key={name}
+                cx={x}
+                cy={y}
+                r={10}
+                fill="transparent"
+                stroke="black"
+              />
+            ))}
+          </g>
+        )}
       </svg>
     </div>
   )
