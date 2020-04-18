db22b46fa8adaf33537676650170c25796532c0b
Thomas Sojka <thsojka@web.de>
2020-04-18 16:13:27 +0200

    Getting Example Data
    Find and extract data.
    
    🎥 Next video: https://youtu.be/-t2ZT9DiXcY

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import './App.css'
 
+const data = {
+  name: 'Animals',
+  children: [
+    {
+      name: 'Fish',
+      children: [
+        { name: 'Marine', children: [{ name: 'Hailbut' }] },
+        { name: 'Fresh Water', children: [{ name: 'Rainbow trout' }] }
+      ]
+    },
+    {
+      name: 'Mammals',
+      children: [{ name: 'Dog' }, { name: 'Cat' }, { name: 'Mouse' }]
+    }
+  ]
+}
+
 function App() {
   return (
     <div style={{ paddingLeft: '2rem' }}>
