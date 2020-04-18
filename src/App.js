7dba44b48561673830007ca0d3595682f7f1aada
Thomas Sojka <thsojka@web.de>
2020-04-18 16:16:54 +0200

    Create a Pack Visualization
    Learn about the pack visualization API and create your own.
    
    🎥 Next video: https://youtu.be/G2WY7u7LljI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import { pack, hierarchy } from 'd3-hierarchy'
 
 const data = {
   name: 'Animals',
@@ -18,10 +19,28 @@ const data = {
   ]
 }
 
+const animalsHierarchy = () => hierarchy(data).sum(() => 1)
+const createPack = pack()
+  .size([500, 500])
+  .padding(20)
+const animalsPack = createPack(animalsHierarchy()).descendants()
+
 function App() {
   return (
     <div style={{ paddingLeft: '2rem' }}>
       <h1>Morphing Viz with D3.js & React</h1>
+      <svg width="500" height="500">
+        {animalsPack.map(({ x, y, r, data: { name } }) => (
+          <circle
+            key={name}
+            cx={x}
+            cy={y}
+            r={r}
+            fill="transparent"
+            stroke="black"
+          />
+        ))}
+      </svg>
     </div>
   )
 }
